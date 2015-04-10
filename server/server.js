8b2b90265aabaf8800b81d946fdee85cc0487874
Ritchie Martori <skawful@gmail.com>
2015-04-10 14:51:20 -0700

    Add custom error example

--- server/server.js
+++ server/server.js
@@ -19,3 +19,23 @@ app.start = function() {
 if (require.main === module) {
   app.start();
 }
+
+app.get('remoting').errorHandler = {
+  handler: function(err, req, res, defaultHandler) {
+    err = app.buildError(err);
+
+    // send the error back to the original handler
+    defaultHandler(err);
+  },
+  disableStackTrace: true
+}
+
+app.buildError = function(err) {
+  err.message = 'Custom message: ' + err.message;
+  err.status = 408; // override the status
+
+  // remove the statusCode property
+  delete err.statusCode;
+
+  return err;
+}
